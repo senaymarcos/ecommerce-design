675992bfef9da9a641d576ed736b453c4e06b979
senaymarcos <atli.senay@gmail.com>
2022-10-04 21:58:39 +0200

    add something in new page

---
+++ hacker.js
@@ -0,0 +1,20 @@
+var alice=0;
+var bob=0;
+var a=[2,4,6];
+var b=[3,5,6];
+for (let i=0; i<a.length; i++)
+ {
+     if(a[i]<b[i])
+      bob+=1;
+      else if(a[i]>b[i])
+       alice+=1;
+ }
+ console.log(bob +" bob'un puanı");
+ console.log(alice +" alice'in puanı");
+
+
+
+ var sum=0;
+ var number;
+
+ 
